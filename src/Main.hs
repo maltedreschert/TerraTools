1867959040eb143002ec47da6dab846856814bdd
Malte Dreschert <malte.dreschert@gmail.com>
2014-11-07 15:21:19 -0800

    add deserializer. Not working yet

--- src/Main.hs
+++ src/Main.hs
@@ -1,8 +1,22 @@
 -- | Main entry point to the application.
 module Main where
 
+import qualified Data.ByteString.Lazy as B
+import Data.Binary.Get
+import Data.Word
+
+deserializeHeader :: Get (Word8)
+deserializeHeader = do
+    a <- getWord8
+    return (a)
+
+
+
+
 -- | The main entry point.
 main :: IO ()
 main = do
+    terContent <- B.readFile "TestData.ter"
+    print $ runGet deserializeHeader terContent
     putStrLn "Welcome to FP Haskell Center!"
     putStrLn "Have a good day!"
